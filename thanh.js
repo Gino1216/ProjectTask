eb7c33ae9f9e359379d8e5555135b30a358f00c4
tanh214 <doyousay987@Gmail.com>
2024-10-27 22:42:13 +0700

    1

--- thanh.js
+++ thanh.js
@@ -1,13 +1,13 @@
 // script.js
 function toggleContent() {
-    const extraContent = document.getElementById("extraContent");
-    const button = document.querySelector("button");
+    const extraContent = document.getElementById("extraContent1");
+    const button = document.querySelector("buttoncuathanh");
   
-    if (extraContent.classList.contains("hidden")) {
-      extraContent.classList.remove("hidden");
+    if (extraContent.classList.contains("hidden1")) {
+      extraContent.classList.remove("hidden1");
       button.textContent = "Ẩn bớt";
     } else {
-      extraContent.classList.add("hidden");
+      extraContent.classList.add("hidden1");
       button.textContent = "Hiển thị thêm";
     }
   }
