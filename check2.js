0b818af600c5f297ef4bf6d686c62305f1c872d3
hjeprua <122618482+Gino1216@users.noreply.github.com>
2024-10-31 19:03:11 +0700

    update

--- check2.js
+++ check2.js
@@ -334,3 +334,75 @@ document.getElementById("report-form").addEventListener("submit", function(event
 
 // Hiển thị danh sách người khi tải trang
 displayPeople(people);
+
+const menu = [
+    {
+      label: "Trang Chủ",
+      href: "/index",
+    },
+    {
+      label: "Về Chúng Tôi",
+      href: "/about us",
+    },
+    {
+      label: "Quyên Góp",
+      href: "/ACTIVITI",
+    },
+    {
+      label: "Thông Tin",
+      href: "/check",
+    },
+    {
+      label: "Liên Hệ",
+      href: "/contact us",
+    },
+  ];
+  
+  function getMenuMobile() {
+    const containerMenuMobileElement = document.querySelector("#wrapper");
+    const iconMenuMobileElement = document.querySelector(".parent-icon");
+    const iconMenuOpenElement = document.querySelector(".icon-menu-open");
+    const iconMenuCloseElement = document.querySelector(".icon-menu-close");
+  
+    const navElement = document.createElement("nav");
+    navElement.classList.add("nav-list-menu-mobile");
+    const ulElement = document.createElement("ul");
+    ulElement.classList.add("list-menu-mobile");
+  
+    menu.forEach((item) => {
+      const liElement = document.createElement("li");
+      const aElement = document.createElement("a");
+      liElement.classList.add("primary-nav");
+      liElement.classList.add("secondary-nav");
+      aElement.href = item.href + ".html";
+      aElement.textContent = item.label;
+      liElement.appendChild(aElement);
+      ulElement.appendChild(liElement);
+    });
+  
+    navElement.appendChild(ulElement);
+    containerMenuMobileElement.appendChild(navElement);
+  
+    function toggleMenuMobile() {
+      const navElement = document.querySelector(".nav-list-menu-mobile");
+      if (containerMenuMobileElement.classList.contains("showMenu")) {
+        containerMenuMobileElement.style.backgroundColor = "transparent";
+        containerMenuMobileElement.style.height = "0";
+        iconMenuOpenElement.style.display = "block";
+        iconMenuCloseElement.style.display = "none";
+        containerMenuMobileElement.classList.remove("showMenu");
+         navElement.style.display = "none"
+      } else {
+        containerMenuMobileElement.style.backgroundColor = "white";
+        containerMenuMobileElement.style.height = "100%";
+        iconMenuOpenElement.style.display = "none";
+        iconMenuCloseElement.style.display = "block";
+        containerMenuMobileElement.classList.add("showMenu");
+         navElement.style.display = "none"
+        navElement.style.height = "100%";
+        navElement.style.display = "block"
+      }
+    }
+    iconMenuMobileElement.addEventListener("click", toggleMenuMobile);
+  }
+  getMenuMobile();
